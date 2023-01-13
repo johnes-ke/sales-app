2cb9c8d0587346bcd3d49a2f48219fcdd5acb2dc
johnes.mecha <johnes.mecha@phoenix-dimas.com>
2023-01-13 10:39:43 +0300

    Added Angular Material, at Drawer and mat-toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
